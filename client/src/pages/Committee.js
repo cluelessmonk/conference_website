ba221719ea22dde6beaef2031778af2651ee37af
EngineerDivyansh <83111965+EngineerDivyansh@users.noreply.github.com>
2024-04-03 17:30:10 +0530

    Add files via upload

--- client/src/pages/Committee.js
+++ client/src/pages/Committee.js
@@ -1,11 +1,134 @@
 import React from "react";
 import Layout from "../components/Layout";
 import "../styles/OrganizingCommittee.css";
-
 function Committee() {
+  const organizingCommittee = [
+    {
+      role: "Patron",
+      name: "Prof. Rama S. Verma",
+      department: "Director",
+      college: "MNNIT Allahabad",
+    },
+    {
+      role: "Chairperson",
+      name: "Head",
+      department: "Department of Biotechnology",
+      college: "MNNITA",
+    },
+    {
+      role: "Treasurer",
+      name: "Dr. Joybrata Mal",
+      department: "Department of Biotechnology",
+      college: "MNNIT Allahabad",
+    },
+  ];
+
+  const organizingSecretaries = [
+    {
+      name: "Dr. Manisha Sachan",
+      department: "Department of Biotechnology",
+      college: "MNNIT Allahabad",
+    },
+    {
+      name: "Dr. Seema Nara",
+      department: "Department of Biotechnology",
+      college: "MNNIT Allahabad",
+    },
+  ];
+
+  const organizingCoordinators = [
+    {
+      name: "Dr. Rajitha B.",
+      department: "Department of Computer Science and Engineering",
+      college: "MNNIT Allahabad",
+    },
+    {
+      name: "Dr. Girijesh Patel",
+      department: "Department of Biotechnology",
+      college: "MNNIT Allahabad",
+    },
+    {
+      name: "Dr. Nirmal Ch. Roy",
+      department: "Department of Electronics and Communication Engineering",
+      college: "MNNIT Allahabad",
+    },
+  ];
   return (
     <Layout>
-      <div>Committee</div>
+      <div>
+        <header>
+          <h1>Organizing Committee</h1>
+        </header>
+        <main>
+          <div className="table-container">
+            <h2>Organizing Committee</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Role</th>
+                  <th>Name</th>
+                  <th>College</th>
+                </tr>
+              </thead>
+              <tbody>
+                {organizingCommittee.map((member, index) => (
+                  <tr key={index}>
+                    <td>{member.role}</td>
+                    <td>
+                      {member.name} , {member.department}{" "}
+                    </td>
+                    <td>{member.college}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="table-container">
+            <h2>Organizing Secretary(s)</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Department</th>
+                  <th>College</th>
+                </tr>
+              </thead>
+              <tbody>
+                {organizingSecretaries.map((member, index) => (
+                  <tr key={index}>
+                    <td>{member.name}</td>
+                    <td>{member.department}</td>
+                    <td>{member.college}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="table-container">
+            <h2>Organizing Coordinator(s)</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Department</th>
+                  <th>College</th>
+                </tr>
+              </thead>
+              <tbody>
+                {organizingCoordinators.map((member, index) => (
+                  <tr key={index}>
+                    <td>{member.name}</td>
+                    <td>{member.department}</td>
+                    <td>{member.college}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
     </Layout>
   );
 }
--- client/src/pages/Special_Session.js
+++ client/src/pages/Special_Session.js
@@ -4,7 +4,53 @@ import "../styles/SpecialSessions.css";
 function Special_Session() {
   return (
     <Layout>
-      <div>special session</div>
+      <div className="special-sessions-container">
+        <h2>Special Sessions</h2>
+        <div className="session">
+          <h3>
+            SS01: eXplainable Artificial Intelligence (XAI) for Cyber-Physical
+            Systems
+          </h3>
+          <p>
+            <strong>Organizers:</strong>
+          </p>
+          <ul>
+            <li>
+              Dr. Ramanujam E., Assistant Professor, Department of Computer
+              Science and Engineering, National Institute of Technology Silchar,
+              Assam, India - 788010
+            </li>
+            <li>
+              Dr. Abirami A M., Associate Professor, Department of Information
+              Technology, Thiagarajar College of Engineering, Madurai, Tamil
+              Nadu, India - 625015
+            </li>
+          </ul>
+        </div>
+        <div className="session">
+          <h3>
+            SS02: Smart Technologies for Sustainable Energy, Environment and
+            Transportation System
+          </h3>
+          <p>
+            <strong>Organizers:</strong>
+          </p>
+          <ul>
+            <li>
+              Dr. Shailendra Singh, Department of Electrical Engineering,
+              National Institute of Technology Agartala, Tripura
+            </li>
+            <li>
+              Dr. Sadhan Gope, Department of Electrical Engineering, National
+              Institute of Technology Agartala, Tripura
+            </li>
+            <li>
+              Dr. Padmagirisan P., Department of Electrical Engineering,
+              National Institute of Technology Agartala, Tripura
+            </li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
